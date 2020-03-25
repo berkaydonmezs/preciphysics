cff7891f54c5df7f486c2c7a5d63c7ac7c928eaa
donmezberkay <donmezberkay@outlook.com>
2020-03-25 23:28:12 +0300

    upload

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
-from distutils.core import setup
-setup(name='preciphysics',
-      version='1.0',
-      author='Berkay DONMEZ',
-      packages=['preciphysics'],
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="preciphysics",
+    version="0.0.1",
+    author="Berkay DONMEZ",
+    author_email="donmezberkay@outlook.com",
+    description="Numerical calculations in Meteorology",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/donmezberkay/preciphysics",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: MIT License",
+    ],
 )
